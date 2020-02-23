3cd1807e6b4aa9c8c8ac148b50a8febab33df93c
JJMARKL <jjmarkl92@gmail.com>
2020-02-23 17:26:38 -0500

    N/a

---
+++ JS/example.js
@@ -0,0 +1,20 @@
+var chest = 90;
+var lats = 81;
+var shoulders = 75;
+var traps = 68;
+var rear_delts = 73;
+var biceps = 84;
+var triceps = 87;
+var forearms = 57;
+
+var abdominals = 51;
+var obliques = 48;
+
+var quads = 71;
+var hamstrings = 51;
+var glutes = 41;
+var hips_groin = 44;
+var calves = 82;
+
+var endurance = 56;
+var agility = 63;
---
+++ JS/example0.js
@@ -0,0 +1,20 @@
+var chest = 43;
+var lats = 48;
+var shoulders = 38;
+var traps = 32;
+var rear_delts = 36;
+var biceps = 51;
+var triceps = 49;
+var forearms = 63;
+
+var abdominals = 74;
+var obliques = 68;
+
+var quads = 70;
+var hamstrings = 65;
+var glutes = 52;
+var hips_groin = 75;
+var calves = 82;
+
+var endurance = 87;
+var agility = 78;
